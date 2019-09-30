9bbae39d3066e2f1f0dfbd87a4af4eb18ef63978
Tatiana Klimenko <tk@pale.blue>
2019-09-30 14:40:14 +0300

    Add sort logic

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using QuickSort.Core;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] array = new int[100];
+            for (int i = 0; i < array.Length; array[i++] = r.Next(30)) ;
+
+            int[] arr2 = new int[100];
+            array.CopyTo(arr2, 0);
+            Array.Sort(arr2);
+            Utility.PrintArray(arr2);
+
+            Utility.PrintArray(array);
+            Sort.QuickSortRecursive(array, 0, array.Length - 1);
+            Utility.PrintArray(array);
+
+
+            Console.WriteLine(arr2.SequenceEqual(array));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSort/QuickSort/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuickSort
+{
+    public static class Utility
+    {
+        public static void PrintArray<T>(T[] array)
+        {
+            string s = "";
+            foreach (T t in array)
+            {
+                s += $" {t}";
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
